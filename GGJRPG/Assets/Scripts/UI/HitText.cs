0ade15e7b79a6517ced8acbfe0fedd6975fd54e6
dougdemyen <doug.demyen@gmail.com>
2017-01-21 16:35:34 -0400

    First pass at UI

---
+++ GGJRPG/Assets/Scripts/UI/HitText.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HitText : MonoBehaviour
+{
+    public float Lifetime;
+    public float Height;
+
+    private float m_fRemaining;
+    private Image m_CImage;
+    private RectTransform m_CTransform;
+
+    private void Start()
+    {
+        m_fRemaining = Lifetime;
+        m_CImage = GetComponent<Image>();
+        m_CTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        m_fRemaining -= Time.deltaTime;
+        if (m_fRemaining <= 0.0f)
+        {
+            Object.Destroy(gameObject);
+        }
+        else
+        {
+            if (m_CImage != null)
+            {
+                m_CImage.color = new Color(m_CImage.color.r, m_CImage.color.g, m_CImage.color.b, m_fRemaining / Lifetime);
+            }
+            if (m_CTransform != null)
+            {
+                m_CTransform.anchoredPosition = new Vector2(m_CTransform.anchoredPosition.x, (1.0f - m_fRemaining) / Lifetime * Height);
+            }
+        }
+    }
+}
---
+++ GGJRPG/Assets/Scripts/UI/MessageManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MessageManager : MonoBehaviour
+{
+    public float Lifetime;
+    public float FadeTime;
+
+    public Text MessageText;
+    public Image BackgroundImage;
+
+    private float m_fRemaining;
+
+    protected static MessageManager m_sInstance = null;
+
+    public static MessageManager Instance
+    {
+        get
+        {
+            return m_sInstance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (m_sInstance == null)
+        {
+            m_sInstance = this;
+        }
+        else
+        {
+            Debug.LogWarning("An instance of MessageManager already exists");
+        }
+    }
+
+    public void SetMessage(string a_strMessage)
+    {
+        if (MessageText != null)
+        {
+            MessageText.text = a_strMessage;
+            MessageText.color = new Color(MessageText.color.r, MessageText.color.g, MessageText.color.b, 1.0f);
+        }
+        if (BackgroundImage != null)
+        {
+            BackgroundImage.color = new Color(BackgroundImage.color.r, BackgroundImage.color.g, BackgroundImage.color.b, 1.0f);
+        }
+        m_fRemaining = Lifetime + FadeTime;
+    }
+
+    private void Update()
+    {
+        m_fRemaining -= Time.deltaTime;
+        float l_fAlpha = (m_fRemaining <= 0.0f) ? 0.0f : (m_fRemaining >= FadeTime) ? 1.0f : m_fRemaining / FadeTime;
+        if (MessageText != null)
+        {
+            MessageText.color = new Color(MessageText.color.r, MessageText.color.g, MessageText.color.b, l_fAlpha);
+        }
+        if (BackgroundImage != null)
+        {
+            BackgroundImage.color = new Color(BackgroundImage.color.r, BackgroundImage.color.g, BackgroundImage.color.b, l_fAlpha);
+        }
+    }
+}
