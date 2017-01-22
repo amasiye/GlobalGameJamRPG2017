df9bc658374aa1e105a080f6e29d8e789f84c391
dougdemyen <doug.demyen@gmail.com>
2017-01-21 20:58:15 -0400

    Bind panel.

---
+++ GGJRPG/Assets/Scripts/UI/BindIcon.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BindIcon : MonoBehaviour
+{
+    public Character.Element ElementType;
+    public Text LabelText;
+    public Image ElementIcon;
+
+    public Color Default;
+    public Color Selected;
+    public Color Highlighted;
+    public Color HighlightedSelected;
+
+    private bool m_bHighlighted;
+    private bool m_bSelected;
+
+    public void SetHighlighted(bool a_bHighlighted)
+    {
+        m_bHighlighted = a_bHighlighted;
+        UpdateStyle();                
+    }
+
+    public void SetSelected(bool a_bSelected)
+    {
+        m_bSelected = a_bSelected;
+        UpdateStyle();
+    }
+
+    public bool GetSelected()
+    {
+        return m_bSelected;
+    }
+
+    private void UpdateStyle()
+    {
+        if (LabelText != null)
+        {
+            LabelText.fontStyle = (m_bHighlighted) ? FontStyle.Bold : FontStyle.Normal;
+        }
+        if (ElementIcon != null)
+        {
+            ElementIcon.color = (m_bHighlighted && m_bSelected) ? HighlightedSelected
+                : (m_bHighlighted) ? Highlighted : (m_bSelected) ? Selected : Default;
+        }
+    }
+}
