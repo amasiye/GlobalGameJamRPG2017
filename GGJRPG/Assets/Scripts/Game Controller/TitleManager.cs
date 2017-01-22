1813ce1ddc8d79c86114dcc726009a196550ae92
Andrew Masiye <amasiye313@gmail.com>
2017-01-22 11:20:53 -0400

    Add Title Scene
    Add Title Scene
    Add Title theme background music

---
+++ GGJRPG/Assets/Scripts/Game Controller/TitleManager.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TitleManager : MonoBehaviour
+{
+    public AudioClip mainTheme;
+    public AudioClip sfxAccept;
+
+    public float fadeTime = 0.1f;
+    private float nextTime = 0f;
+    public Image bgBlackImage;
+    public Color startColor;
+    public Color endColor;
+
+    public float loadingTime = 2.0f;
+
+    public enum SceneState { Init, Prompt, Loading }
+    public SceneState titleSceneState = SceneState.Init;
+
+    [SerializeField]
+    private Blinker promptBlinker;
+
+    void Start()
+    {
+        titleSceneState = SceneState.Init;
+        nextTime = Time.time + fadeTime;
+    }
+
+    void Update()
+    {
+        switch(titleSceneState)
+        {
+            case SceneState.Init:
+                bgBlackImage.color = Color.Lerp(startColor, endColor, Time.time);
+                if(Time.time > nextTime)
+                    titleSceneState = SceneState.Prompt;
+                break;
+            case SceneState.Prompt:
+                WaitForInput();
+                break;
+            case SceneState.Loading:
+                break;
+        } 
+    } // end Update()
+
+    void Init()
+    {
+
+    }
+
+    void WaitForInput()
+    {
+        if(!promptBlinker.blink)
+        {
+            promptBlinker.StartBlink();
+        }
+
+        if(Input.GetButtonDown(ButtonManager.xboxStart))
+        {
+            Debug.Log("Hello");
+            if(promptBlinker.blink)
+            {
+                promptBlinker.Halt();
+                LoadGame();
+            }
+        }
+    } // end WaitForInput()
+
+    void LoadGame()
+    {
+        titleSceneState = SceneState.Loading;
+        StartCoroutine(StartGame());
+    } // end LoadGame()
+
+    IEnumerator StartGame()
+    {
+        yield return new WaitForSeconds(loadingTime);
+        SceneManager.LoadScene(1);
+    } // end StartGame()
+    
+}
---
+++ GGJRPG/Assets/Scripts/UI/Blinker.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Blinker : MonoBehaviour
+{
+    public float blinkRate = 1f;
+    public float nextTime = 0f;
+
+    public bool blink = false;
+
+    [SerializeField]
+    private Text text;
+
+    void Start()
+    {
+        if(!text)
+            text = GetComponent<Text>();
+
+        nextTime = Time.time + blinkRate;
+    }
+
+    void Update()
+    {
+        if(blink)
+        {
+            Blink();
+        }
+    } // end Update()
+
+    public void StartBlink()
+    {
+        blink = true;
+    } // end StartBlink()
+
+    void Blink()
+    {
+        if(Time.time > nextTime)
+        {
+            text.enabled = !text.enabled;
+            nextTime = Time.time + blinkRate;
+        }
+    } // end Blink()
+
+    public void Halt()
+    {
+        text.enabled = true;
+        blink = false;
+    } // end Halt()
+}
